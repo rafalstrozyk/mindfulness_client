4451d55890f89fe9fc8163fe37a310e800fd86f6
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2022-01-11 20:10:18 +0100

    create global style with css reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Root from './pages/Root';
+import GlobalStyle from './GlobalStyle';
+
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Root />
     </BrowserRouter>
   );
