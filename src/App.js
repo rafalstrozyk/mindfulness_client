51fdf0d474b5daa53372a8de06bbc197134a580a
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2022-01-26 20:53:38 +0100

    auth graphql with apollo client not work yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React, { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Root from './pages/Root';
 import api from './apis/api';
 import { setToken } from './redux/Slice/userSlice';
 import GlobalStyle from './GlobalStyle';
 import { theme } from './theme';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/gql',
+});
+
+const authLink = setContext((_, headers) => {
+  const token = localStorage.getItem('refreshToken');
+  console.log('cookie', document.cookie);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   const dispatch = useDispatch();
@@ -29,12 +57,15 @@ const App = () => {
 
   return (
     <div style={{ height: '100vh' }}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Root />
-        </ThemeProvider>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          {console.log(localStorage)}
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Root />
+          </ThemeProvider>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useQuery, gql } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import LoginRegister from '../components/LoginRegister/LoginRegister';
+// import api from '../apis/api';
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    getAllPosts {
+      id
+      title
+    }
+  }
+`;
 
 const Home = () => {
   const token = useSelector((state) => state.user.token);
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  //   const getPosts = `
+  //   query someTest {
+  //     getAllPosts {
+  //     id,
+  //     title,
+  //   }
+  // }
+  //   `;
+
   return (
     <>
+      {console.log('data', data)}
+      {console.log('loading', loading)}
+      {console.log('error', error)}
       {token === null ? (
         <LoginRegister />
       ) : token ? (
         <div>
           <h1>Home</h1>
+
           <Link to="/profile">Profile</Link>
         </div>
       ) : (
